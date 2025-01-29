bec23383665495cfdbd832aab101ff91daf16392
Kaloyan Georgiev <webdevkalo@gmail.com>
2025-01-29 13:31:30 +0100

    avatar changed

--- client/src/features/portfolio/components/PortfolioHeader/PortfolioHeader.tsx
+++ client/src/features/portfolio/components/PortfolioHeader/PortfolioHeader.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Edit, Eye, Heart, Save } from 'lucide-react';
+import { Eye, Heart } from 'lucide-react';
 import { useState } from 'react';
 
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 import clsx from 'clsx';
 
-import { Button } from '@/components/ui/button';
+import { Avatar } from '@/components/ui/avatar';
 
 import { PortfolioTitle } from './PortfolioTitle';
 import { SocialLinks } from './SocialLinks';
@@ -13,12 +14,7 @@ import { SocialLinks } from './SocialLinks';
 import type { Portfolio } from '../../types/Portfolio';
 
 export const PortfolioHeader = ({ portfolio }: { portfolio: Portfolio }) => {
-  const [isEditing, setIsEditing] = useState<boolean>(false);
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
-
-  const handleSave = () => {
-    setIsEditing(false);
-  };
 
   const toggleFavorite = () => {
     setIsFavorite((prev) => !prev);
@@ -26,17 +22,17 @@ export const PortfolioHeader = ({ portfolio }: { portfolio: Portfolio }) => {
   };
 
   return (
-    <header className="bg-slate-800 py-8 text-primary-foreground">
+    <header className="bg-slate-800 h-[600px] py-16 text-primary-foreground">
       <div className="container">
         <div className="mb-4 flex items-center justify-between">
           <div className="flex items-center space-x-4">
-            <img
-              src={portfolio.avatar || '/placeholder.svg'}
-              alt={portfolio.author}
-              width={60}
-              height={60}
-              className="rounded-full"
-            />
+            <Avatar className="h-32 w-32">
+              <AvatarImage
+                src={portfolio.avatar || 'https://github.com/shadcn.png'}
+                alt={portfolio.author}
+              />
+              <AvatarFallback>Pic</AvatarFallback>
+            </Avatar>
             <div>
               <PortfolioTitle title={portfolio.title} />
               <p className="text-xl">{portfolio.author}</p>
