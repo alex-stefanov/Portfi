a70485c97432c388c8be142e087c631e37938c8e
Aleks Stefanov <astefanov@david.bg>
2025-01-29 14:21:53 +0200

    Added Extensions folder in the Core layer and changed the Program.cs file to handle memory and to register the services and repositories. Other file changes

--- core/Portfi.Core/Program.cs
+++ core/Portfi.Core/Program.cs
@@ -1,4 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Portfi.Core.Extensions;
+using DATA = Portfi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var environment = builder.Environment;
+
+builder.Configuration
+    .AddEnvironmentSpecificJsonFiles(environment, out string connectionString);
+
+builder.Services
+    .AddDbContext<DATA.PortfiDbContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
+
+builder.Services
+    .RegisterRepositories()
+    .RegisterUserDefinedServices();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
