31039ce05817ff6445c4359667bc3a5c44c8a9a8
Aleks Stefanov <astefanov@david.bg>
2025-01-29 17:04:26 +0200

    Added ErrorResponse model

---
+++ core/Portfi.Infrastructure/Models/Responses/ErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfi.Infrastructure.Models.Responses;
+
+/// <summary>
+/// Represents an error response.
+/// </summary>
+public record class ErrorResponse
+{
+    /// <summary>
+    /// Message explaining the error.
+    /// </summary>
+    [JsonPropertyName("message")]
+    public required string Message { get; set; }
+
+    /// <summary>
+    /// When the error occurred.
+    /// </summary>
+    [JsonPropertyName("date")]
+    [DataType(DataType.DateTime)]
+    public required DateTime Date { get; set; }
+}
