b0d4354cdef1091e3c94dc988b50f934a880755b
alex-stefanov <rlgalexbgto@gmail.com>
2025-02-20 10:54:44 +0200

    Made endpoints RESTfull

---
+++ core/Portfi.Infrastructure/Models/Requests/ThemeUpdateRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Portfi.Infrastructure.Models.Requests;
+
+public class ThemeUpdateRequest
+{
+    public string? BackgroundTheme { get; set; }
+    public string? MainColor { get; set; }
+}
